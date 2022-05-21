ae1b67577e98f6cd6ad224b880f9388fab0b5a69
Michał <sukowskimichal@gmail.com>
2022-05-21 23:52:04 +0200

    In build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName = "";
+  members: string[] = [];
+
+  addMember() {
+    console.log("Clicked");
+  };
+
+  handleInputChange(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
