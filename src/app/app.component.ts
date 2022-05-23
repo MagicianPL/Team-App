c80be392d4cf717da54410fb0393ecb8fe2a8fe3
Michał <sukowskimichal@gmail.com>
2022-05-23 12:49:52 +0200

    Working app!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { ignoreElements } from 'rxjs';
 })
 export class AppComponent {
   newMemberName = "";
-  members: string[] = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  members: string[] = [];
   errorMessage = "";
   numOfTeams="";
   teams: string[][] = [];
