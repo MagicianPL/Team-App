4f9a848a292410d3d8058be0a998f1b64c1ec2d5
Michał <sukowskimichal@gmail.com>
2022-05-23 11:06:18 +0200

    generateTeams is working!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newMemberName = "";
-  members: string[] = [];
+  members: string[] = ["1", "2", "3", "4", "5", "6", "7", "8"];
   errorMessage = "";
   numOfTeams="";
+  teams: any = [];
 
   addMember() {
     if(!this.newMemberName.trim()) {
@@ -28,9 +29,24 @@ export class AppComponent {
   }
 
   generateTeams() {
-    if(!this.numOfTeams.trim()) return this.errorMessage = "Number of teams cannot be empty!";
+    //Validation
+    if(!this.numOfTeams.toString().trim()) return this.errorMessage = "Number of teams cannot be empty!";
     if(+this.numOfTeams > this.members.length) return this.errorMessage = "Number of teams is too big.";
 
-    
+    //Push empty arrays to teams
+    for(let i = 0; i < +this.numOfTeams; i++) {
+      this.teams.push([]);
+    };
+    //Take random member and push them to the teams
+    do {
+      for(let i = 0; i < this.teams.length; i++) {
+        const randomIndexOfNewMember = Math.floor(Math.random() * this.members.length);
+        const removedMember = this.members.splice(randomIndexOfNewMember, 1);
+        if(removedMember[0]) {
+          this.teams[i].push(removedMember[0]);
+        }
+      }
+    } while(this.members.length > 0);
+    return;
   };
 }
